9946cae0b7d2178f219123e652dfac1ba41f6436
denisshashkov <shashkov-56@mail.ru>
2020-06-03 18:14:50 +0300

    add homepage url

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ if (process.env.NODE_ENV === "development") {
   require("file-loader!./index.pug");
 }
 
+localStorage.setItem('homePage', location.href);
+
 import "./scripts/skills";
 import "./scripts/parallax";
 import "./scripts/slider";
